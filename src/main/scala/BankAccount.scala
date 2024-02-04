0049c5a42616b64a7051e2b2db80d4e8eb8483d0
Christopher Spears <cspears2002@gmail.com>
2024-02-04 15:22:39 -0800

    Create SavingsAccount

--- src/main/scala/BankAccount.scala
+++ src/main/scala/BankAccount.scala
@@ -8,7 +8,6 @@ package accounts:
 
     
     class CheckingAccount(initialBalance: Double) extends BankAccount(initialBalance):
-
         private val charge: Double = 1
         override def deposit(amount: Double): Double = {
             super.deposit(amount - charge)
@@ -16,4 +15,35 @@ package accounts:
 
         override def withdraw(amount: Double): Double = {
             super.withdraw((amount + charge))
+        }
+
+    
+    class SavingsAccount(initialBalance: Double) extends BankAccount(initialBalance):
+        private val interest: Double = 0.05
+        private val charge: Double = 1
+        private var freeDeposits: Int = 3
+        private var freeWithdrawals: Int = 3
+
+        override def deposit(amount: Double): Double = {
+            if freeDeposits != 0 then
+                freeDeposits -= 1
+                super.deposit(amount)
+            else
+                super.deposit(amount - charge)
+        }
+
+        override def withdraw(amount: Double): Double = {
+            if freeWithdrawals != 0 then
+                freeWithdrawals -= 1
+                super.withdraw(amount)
+            else
+                super.withdraw((amount + charge))
+        }
+
+        def earnMonthInterest(): Double = {
+            freeDeposits = 3
+            freeWithdrawals = 3
+
+            var balance = super.currentBalance
+            balance + (balance * interest)
         }
