666d9495647520d1e149c2e4f579b9c49bbeed55
Christopher Spears <cspears2002@gmail.com>
2024-02-25 11:11:22 -0800

    User can update year and license plate

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,5 +10,12 @@ import cars.*
 
   val myCar = Car("Toyota", "Prius", 2016, "MYCAR")
   println(s"${myCar.description}")
- 
+
+  val myCar1 = Car("Toyota", "Prius")
+  println(s"${myCar1.description}")
+  myCar1.year = 2016
+  myCar1.license = "MYCAR"
+  println(s"${myCar1.description}")
+
+
 def msg = "I was compiled by Scala 3. :)"
