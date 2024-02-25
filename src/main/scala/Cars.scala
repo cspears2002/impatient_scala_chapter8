5d9d50c82202ac724e54e7e3123adbb0f1c242d3
Christopher Spears <cspears2002@gmail.com>
2024-02-25 11:08:32 -0800

    Clean up formatting

--- src/main/scala/Cars.scala
+++ src/main/scala/Cars.scala
@@ -1,8 +1,8 @@
 package cars:
     class Car(manufacturer: String,
-            model: String, 
-            year: Double, 
-            license: String):
+              model: String, 
+              year: Double = -1, 
+              license: String = ""):
         
         def description = 
             s"${manufacturer} ${model} ${year} license plate: ${license}"
