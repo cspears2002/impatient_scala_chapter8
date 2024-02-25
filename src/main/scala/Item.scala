582597f12765c939268e3151c0f82647eac63a88
Christopher Spears <cspears2002@gmail.com>
2024-02-25 11:44:57 -0800

    Created subclasses of Item abstract class

--- src/main/scala/Item.scala
+++ src/main/scala/Item.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable.ArrayBuffer
+
 package item:
 
     abstract class Item:
@@ -7,3 +9,14 @@ package item:
     class SimpleItem(myPrice: Double, myDesc: String) extends Item:
         def price: Double = myPrice
         def description: String = myDesc
+
+    class Bundle(bundleDesc: String) extends Item:
+        var itemsArrayBuff = ArrayBuffer[Item]()
+        var price: Double =
+            var sum = 0.0
+            for (item <- itemsArrayBuff) {
+                sum = sum + item.price
+            }
+            price
+
+        def description: String = bundleDesc
