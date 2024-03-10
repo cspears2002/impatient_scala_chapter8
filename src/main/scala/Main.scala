746dfa02e4016d4e2e6d8534176ce79a3e88e7a5
Christopher Spears <cspears2002@gmail.com>
2024-03-10 13:46:32 -0700

    Create a abstract class called Shape

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,7 @@ import accounts.*
 import cars.*
 import items.*
 import points.*
+import shape.*
 
 @main def hello(): Unit =
   val item1 = SimpleItem(0.99, "Item 1")
@@ -13,6 +14,13 @@ import points.*
 
   val origin = LabeledPoint(0.0, 0.0, "origin")
   print(s"Origin X: ${origin.x} Origin Y: ${origin.y} Label: ${origin.label}\n")
+
+  val circle = Circle()
+  val circleCenter = circle.centerPoint(Point(-2, 4), Point(6, 16))
+  print(s"Circle center: (${circleCenter.x}, ${circleCenter.y})\n")
+  val rectangle = Rectangle()
+  val rectCenter = rectangle.centerPoint(Point(-2, 4), Point(6, 16))
+  print(s"Rectangle center: (${rectCenter.x}, ${rectCenter.y})\n")
 
 
 def msg = "I was compiled by Scala 3. :)"
---
+++ src/main/scala/Shape.scala
@@ -0,0 +1,25 @@
+package shape:
+    import points.*
+
+    abstract class Shape:
+        def centerPoint(point1: Point, point2: Point): Point
+
+    class Rectangle extends Shape:
+        override def centerPoint(point1: Point, point2: Point): Point = 
+            val x1 = point1.x
+            val y1 = point1.y
+            val x2 = point2.x
+            val y2 = point2.y
+            val xCenter = (x1 + x2) / 2
+            val yCenter = (y1 + y2) / 2
+            Point(xCenter, yCenter)
+
+    class Circle extends Shape:
+        override def centerPoint(point1: Point, point2: Point): Point =
+            val x1 = point1.x
+            val y1 = point1.y
+            val x2 = point2.x
+            val y2 = point2.y
+            val xCenter = (x1 + x2) / 2
+            val yCenter = (y1 + y2) / 2
+            Point(xCenter, yCenter)
