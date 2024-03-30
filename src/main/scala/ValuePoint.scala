4d52d580487a3e1f02240f1586cf8435c710ff97
Christopher Spears <cspears2002@gmail.com>
2024-03-30 16:09:29 -0700

    Rename Point to ValPoint

--- src/main/scala/ValuePoint.scala
+++ src/main/scala/ValuePoint.scala
@@ -3,11 +3,11 @@ package valuepoint:
     import scala.compiletime.ops.long
 
     // We are assuming an upper bound of 100 for the first coordinate.
-    
-    class Point(val coords: (Int, Int)) extends AnyVal:
-        def n = coords(0) * 100 + coords(1)
 
-    object Point:
-        def apply(coords: (Int, Int)): Point =
-            if coords(0) < 100 then Point(coords)
+    class ValPoint(val coords: (Int, Int)) extends AnyVal:
+        private def n = coords(0) * 100 + coords(1)
+
+    object ValPoint:
+        def apply(coords: (Int, Int)): ValPoint =
+            if coords(0) < 100 then ValPoint(coords)
             else throw IllegalArgumentException()
